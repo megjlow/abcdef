a06fc79880944928114603b75094d181f2034f03
megjlow <m.j.low@warwick.ac.uk>
2016-06-26 16:08:22 +0100

    trial again

--- extension.js
+++ extension.js
@@ -79,7 +79,7 @@ url: 'https://gist.githubusercontent.com/anonymous/1ec6455373e830b14909/raw/317c
 			complete: function() {
 				alert("complete");
 			}
-		})
+		});
 		//window.open("http://" + url)
 	};
 
